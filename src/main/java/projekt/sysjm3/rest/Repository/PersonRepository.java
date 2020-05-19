681fb61fb8a8d0790e464bfc3b88f17267b9b80b
Sigge Cinnamon <SiggeCinnamon@gmail.com>
2020-05-19 14:00:24 +0200

    Fixed typo

--- src/main/java/projekt/sysjm3/rest/Repository/PersonRepository.java
+++ src/main/java/projekt/sysjm3/rest/Repository/PersonRepository.java
@@ -1,9 +1,13 @@
 package projekt.sysjm3.rest.Repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
 
 import projekt.sysjm3.rest.Entity.Person;
 
-public interface PersonRepository extends CrudRepository<Person, Integer> {
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
 
 }
