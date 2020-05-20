bd305b8144b228f734ee09510e36bcc67c386b24
Sigge Cinnamon <SiggeCinnamon@gmail.com>
2020-05-20 11:12:37 +0200

    dsfdsf

--- src/main/java/projekt/sysjm3/rest/Entity/Person.java
+++ src/main/java/projekt/sysjm3/rest/Entity/Person.java
@@ -1,30 +1,26 @@
 package projekt.sysjm3.rest.Entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "person")
 public class Person {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	public int id;
-	@Column(name = "lastname", nullable = false)
 	public String lastName;
-	@Column(name = "firstname", nullable = false)
-	public String firstName;
-	@Column(name = "age", nullable = false)
-	public int age;
-	@Column(name = "ssn", nullable = false)
-	public int socialSecurityNumber;
-	@Column(name = "city")
-	public String cityOfResidence;
-	@Column(name = "country")
-	public String countryOfResicence;
+//	@Column(name = "firstname", nullable = false)
+//	public String firstName;
+//	@Column(name = "age", nullable = false)
+//	public int age;
+//	@Column(name = "ssn", nullable = false)
+//	public int socialSecurityNumber;
+//	@Column(name = "city")
+//	public String cityOfResidence;
+//	@Column(name = "country")
+//	public String countryOfResicence;
 
 //	H2 gillar inte enum, oklart varför. 
 //
@@ -52,51 +48,51 @@ public class Person {
 		this.lastName = lastName;
 	}
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public int getSocialSecurityNumber() {
-		return socialSecurityNumber;
-	}
-
-	public void setSocialSecurityNumber(int socialSecurityNumber) {
-		this.socialSecurityNumber = socialSecurityNumber;
-	}
-
-	public String getCityOfResidence() {
-		return cityOfResidence;
-	}
-
-	public void setCityOfResidence(String cityOfResidence) {
-		this.cityOfResidence = cityOfResidence;
-	}
-
-	public String getCountryOfResicence() {
-		return countryOfResicence;
-	}
-
-	public void setCountryOfResicence(String countryOfResicence) {
-		this.countryOfResicence = countryOfResicence;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [lastName=" + lastName + ", firstName=" + firstName + ", age=" + age + ", socialSecurityNumber="
-				+ socialSecurityNumber + ", cityOfResidence=" + cityOfResidence + ", countryOfResicence="
-				+ countryOfResicence + "]";
-	}
+//	public String getFirstName() {
+//		return firstName;
+//	}
+//
+//	public void setFirstName(String firstName) {
+//		this.firstName = firstName;
+//	}
+//
+//	public int getAge() {
+//		return age;
+//	}
+//
+//	public void setAge(int age) {
+//		this.age = age;
+//	}
+//
+//	public int getSocialSecurityNumber() {
+//		return socialSecurityNumber;
+//	}
+//
+//	public void setSocialSecurityNumber(int socialSecurityNumber) {
+//		this.socialSecurityNumber = socialSecurityNumber;
+//	}
+//
+//	public String getCityOfResidence() {
+//		return cityOfResidence;
+//	}
+//
+//	public void setCityOfResidence(String cityOfResidence) {
+//		this.cityOfResidence = cityOfResidence;
+//	}
+//
+//	public String getCountryOfResicence() {
+//		return countryOfResicence;
+//	}
+//
+//	public void setCountryOfResicence(String countryOfResicence) {
+//		this.countryOfResicence = countryOfResicence;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Person [lastName=" + lastName + ", firstName=" + firstName + ", age=" + age + ", socialSecurityNumber="
+//				+ socialSecurityNumber + ", cityOfResidence=" + cityOfResidence + ", countryOfResicence="
+//				+ countryOfResicence + "]";
+//	}
 
 }
