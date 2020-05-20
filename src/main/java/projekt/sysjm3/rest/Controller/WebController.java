d153e723d973aaeceb013bdeea7b93150838c457
Sigge Cinnamon <SiggeCinnamon@gmail.com>
2020-05-20 13:27:48 +0200

    Beta Bransch

--- src/main/java/projekt/sysjm3/rest/Controller/WebController.java
+++ src/main/java/projekt/sysjm3/rest/Controller/WebController.java
@@ -1,11 +1,11 @@
 package projekt.sysjm3.rest.Controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import projekt.sysjm3.rest.Entity.Person;
@@ -17,14 +17,58 @@ public class WebController {
 	@Autowired
 	PersonRepository PersonRepository;
 
-	@RequestMapping(value = "/")
+	@GetMapping("/login")
+	public String login() {
+		// add loginPage And SpringSecurity
+		return null;
+	}
+
+	@ResponseBody
+	@GetMapping("/")
 	public String home() {
+		return "index";
+
+	}
+
+	@ResponseBody
+	@GetMapping("*")
+	public String fyrahundrafyra() {
+		return "error";
+	}
+
+	@GetMapping("/signup")
+	public String formToAddPerson(Person person) {
+		return "add-user";
+
+	}
+
+	@PostMapping("/addPerson")
+	public String addPerson(Person person, Model model) {
+		PersonRepository.save(person);
+		model.addAttribute("person", PersonRepository.findAll());
+		return "index";
+
+	}
+
+	@GetMapping("/edit/{id}")
+	public String showUpdatePerson(@PathVariable("id") int id, Model model) {
+		Person person = PersonRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Wrong " + id));
+		model.addAttribute("person", person);
+		return "update-Person";
+	}
+
+	@PostMapping("/update/{id}")
+	public String updatePerson(@PathVariable("id") int id, Person person, Model model) {
+		PersonRepository.save(person);
+		model.addAttribute("person", PersonRepository.findAll());
 		return "index";
 	}
 
-	@GetMapping("/listPerson")
-	public Iterable<Person> listAllPersons() {
-
-		return PersonRepository.findAll();
+	@GetMapping("/delete/{id}")
+	public String deletePerson(@PathVariable("id") int id, Model model) {
+		Person person = PersonRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Wrong " + id));
+		PersonRepository.delete(person);
+		model.addAttribute("person", PersonRepository.findAll());
+		return "index";
 	}
 }
