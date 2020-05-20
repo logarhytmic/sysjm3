260ab1d8abe920779cda906d178c255baa0985ae
Sigge Cinnamon <SiggeCinnamon@gmail.com>
2020-05-20 11:12:37 +0200

    dsfdsf

--- src/main/java/projekt/sysjm3/rest/Controller/WebController.java
+++ src/main/java/projekt/sysjm3/rest/Controller/WebController.java
@@ -1,10 +1,30 @@
 package projekt.sysjm3.rest.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import projekt.sysjm3.rest.Entity.Person;
+import projekt.sysjm3.rest.Repository.PersonRepository;
 
 @RestController
 public class WebController {
 
-// Sussi testar att pusha	
-	
+	@Autowired
+	PersonRepository PersonRepository;
+
+	@RequestMapping(value = "/")
+	public String home() {
+		return "index";
+	}
+
+	@GetMapping("/persons")
+	public Iterable<Person> listAllPersons() {
+
+		return PersonRepository.findAll();
+	}
 }
--- src/main/java/projekt/sysjm3/rest/Repository/PersonRepository.java
+++ src/main/java/projekt/sysjm3/rest/Repository/PersonRepository.java
@@ -1,13 +1,13 @@
 package projekt.sysjm3.rest.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.io.Serializable;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import projekt.sysjm3.rest.Entity.Person;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-
+public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
