16c952652da090a327dbca6bf35641d95ab9b774
SiggeCinnamon <mattias.andersen@gmail.com>
2020-06-04 15:48:31 +0200

    Add a pic.

--- src/main/java/projekt/sysjm3/rest/Controller/WebController.java
+++ src/main/java/projekt/sysjm3/rest/Controller/WebController.java
@@ -3,21 +3,24 @@ package projekt.sysjm3.rest.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class WebController {
 
 	/**
 	 * Sets index.html to be the startiong page.
+	 * 
 	 * @return
 	 */
 	@GetMapping("/")
 	public String home() {
-	return "index";
+		return "index";
 	}
-  	@GetMapping ("/about")
+
+	/**
+	 * @return
+	 */
+	@GetMapping("/about")
 	public String about() {
 		return "about";
-	
+	}
 }
-
